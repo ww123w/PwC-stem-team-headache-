ce2fda550631a1d08e99cb0c7067c540a0529a0e
ww123w <wonghinfung0325@gmail.com>
2020-07-18 08:05:25 -0700

    pull

---
+++ Data/Stock(correlation).py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#data
+Stock_price = pd.read_csv('0293.HK copy.csv')
+Passenger = pd.read_csv('HK_aircraft_til2020Jun.csv')
+
+#Visualize the data
+plt.figure(figsize=(12.5,4.5))
+plt.plot(Stock_price['Adj Close'], label='CX')
+#plt.plot(Passenger['Total'], label='Total Passenger')
+plt.title('Relationship Between Stock Price and Total Number of Passengers')
+plt.xlabel('Time Period')
+plt.ylabel('...')
+plt.legend(loc='upper left')
+plt.show()
