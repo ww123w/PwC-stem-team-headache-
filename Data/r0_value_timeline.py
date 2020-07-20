55d11dbf0f2103d40fa0fcf81fbf2609f2d05027
ww123w <wonghinfung0325@gmail.com>
2020-07-19 22:34:00 -0700

    update 2

--- Data/r0_value_timeline.py
+++ Data/r0_value_timeline.py
@@ -3,3 +3,19 @@ from matplotlib import pyplot
 series = read_csv('effective_reproductive_local_2.csv', header=0, index_col=0, parse_dates=True, squeeze=True)
 series.plot()
 pyplot.show()
+
+
+
+
+'''Still doesn't work lol'''
+#import pandas as pd
+#import matplotlib.pyplot as plt
+
+#series = pd.read_csv('effective_reproductive_local_2.csv')
+#plt.figure(figsize=(12.5,4.5))
+#plt.plot(series['r0_value'], label='r0 value')
+#plt.title('Effective Reproductive Number in Hong Kong (local)')
+#plt.xlabel('Time Period')
+#plt.ylabel('r0 value')
+#plt.legend(loc='upper left')
+#plt.show()
