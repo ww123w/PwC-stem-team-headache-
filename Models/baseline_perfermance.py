3bbe770269417db461686123452a4316eeef7f50
ww123w <wonghinfung0325@gmail.com>
2020-07-26 23:57:36 -0700

    time series model part two update

--- Models/baseline_perfermance.py
+++ Models/baseline_perfermance.py
@@ -36,7 +36,7 @@ data_std = aircraft[:TRAIN_SPLIT].std()
 aircraft = (aircraft.values - data_mean) / data_std
 
 # Prediction
-past_history = 200
+past_history = 20
 future_target = 0
 x_train, y_train = univariate_data(aircraft, 0, TRAIN_SPLIT, past_history, future_target)
 x_val, y_val = univariate_data(aircraft, TRAIN_SPLIT, None, past_history, future_target)
@@ -96,3 +96,88 @@ lstm.fit(train, epochs=EPOCHS, steps_per_epoch=EVALUATION_INTERVAL, validation_d
 for x, y in val.take(3):
   plot = show_plot([x[0].numpy(), y[0].numpy(), lstm.predict(x)[0]], 0, 'LSTM model')
   plot.show()
+
+#Part 2
+
+features_considered = ['Total_']
+features = series[features_considered]
+features.index = series['Month']
+features.head()
+
+features.plot(subplots=True)
+
+dataset = features.values
+data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+data_std = dataset[:TRAIN_SPLIT].std(axis=0)
+dataset = (dataset-data_mean)/data_std
+
+#Single step model
+
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                      target_size, step, single_step=False):
+  data = []
+  labels = []
+
+  start_index = start_index + history_size
+  if end_index is None:
+    end_index = len(dataset) - target_size
+
+  for i in range(start_index, end_index):
+    indices = range(i-history_size, i, step)
+    data.append(dataset[indices])
+
+    if single_step:
+      labels.append(target[i+target_size])
+    else:
+      labels.append(target[i:i+target_size])
+
+  return np.array(data), np.array(labels)
+
+past_history = 246
+future_target = 12
+STEP = 6    #idk
+
+x_train_single, y_train_single = multivariate_data(dataset, dataset[:, 1], 0,   #Error
+                                                   TRAIN_SPLIT, past_history,
+                                                   future_target, STEP,
+                                                   single_step=True)
+x_val_single, y_val_single = multivariate_data(dataset, dataset[:, 1],
+                                               TRAIN_SPLIT, None, past_history,
+                                               future_target, STEP,
+                                               single_step=True)
+
+train_data_single = tf.data.Dataset.from_tensor_slices((x_train_single, y_train_single))
+train_data_single = train_data_single.cache().shuffle(buffer_size).batch(batch).repeat()
+
+val_data_single = tf.data.Dataset.from_tensor_slices((x_val_single, y_val_single))
+val_data_single = val_data_single.batch(batch).repeat()
+
+single_step_model = tf.keras.models.Sequential()
+single_step_model.add(tf.keras.layers.LSTM(32,
+                                           input_shape=x_train_single.shape[-2:]))
+single_step_model.add(tf.keras.layers.Dense(1))
+
+single_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='mae')
+
+single_step_history = single_step_model.fit(train_data_single, epochs=EPOCHS,
+                                            steps_per_epoch=EVALUATION_INTERVAL,
+                                            validation_data=val_data_single,
+                                            validation_steps=50)
+
+def plot_train_history(history, title):
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  epochs = range(len(loss))
+
+  plt.figure()
+
+  plt.plot(epochs, loss, 'b', label='Training loss')
+  plt.plot(epochs, val_loss, 'r', label='Validation loss')
+  plt.title(title)
+  plt.legend()
+
+  plt.show()
+
+plot_train_history(single_step_history,
+                   'Single Step Training and validation loss')
