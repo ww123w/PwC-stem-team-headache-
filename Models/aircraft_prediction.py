9ffad050e41f2a44baac0564c940864c413b7c12
ww123w <wonghinfung0325@gmail.com>
2020-07-28 10:32:30 -0700

    pull

--- Models/aircraft_prediction.py
+++ Models/aircraft_prediction.py
@@ -2,6 +2,9 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import acf, pacf
+from statsmodels.tsa.arima_model import ARIMA
 import numpy as np
 
 
@@ -52,3 +55,111 @@ def test_stationarity(timeseries):
 
 test_stationarity(sqrt_avg_diff)
 
+ts_log = np.log(sqrt_avg_diff)
+plt.plot(ts_log)
+
+#Moving average
+moving_avg = ts_log.rolling(12).mean
+plt.plot(ts_log)
+plt.plot(moving_avg, color='red')
+
+ts_log_moving_avg_diff = ts_log - moving_avg
+ts_log_moving_avg_diff.head(12)
+
+ts_log_moving_avg_diff.dropna(inplace=True)
+test_stationarity(ts_log_moving_avg_diff)
+
+expwighted_avg = pd.ewma(ts_log, halflife=12)
+plt.plot(ts_log)
+plt.plot(expwighted_avg, color='red')
+
+ts_log_ewma_diff = ts_log - expwighted_avg
+test_stationarity(ts_log_ewma_diff)
+
+#Eliminating Trend and Seasonality
+ts_log_diff = ts_log - ts_log.shift()
+plt.plot(ts_log_diff)
+
+ts_log_diff.dropna(inplace=True)
+test_stationarity(ts_log_diff)
+
+#Decomposing
+decomposition = seasonal_decompose(ts_log)
+
+trend = decomposition.trend
+seasonal = decomposition.seasonal
+residual = decomposition.resid
+
+plt.subplot(411)
+plt.plot(ts_log, label='Original')
+plt.legend(loc='best')
+plt.subplot(412)
+plt.plot(trend, label='Trend')
+plt.legend(loc='best')
+plt.subplot(413)
+plt.plot(seasonal,label='Seasonality')
+plt.legend(loc='best')
+plt.subplot(414)
+plt.plot(residual, label='Residuals')
+plt.legend(loc='best')
+plt.tight_layout()
+
+ts_log_decompose = residual
+ts_log_decompose.dropna(inplace=True)
+test_stationarity(ts_log_decompose)
+
+# Forecasting a Time Series
+lag_acf = acf(ts_log_diff, nlags=20)
+lag_pacf = pacf(ts_log_diff, nlags=20, method='ols')
+#Plot ACF:
+plt.subplot(121)
+plt.plot(lag_acf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Autocorrelation Function')
+#Plot PACF:
+plt.subplot(122)
+plt.plot(lag_pacf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Partial Autocorrelation Function')
+plt.tight_layout()
+
+#AR Model
+model = ARIMA(ts_log, order=(2, 1, 0))
+results_AR = model.fit(disp=-1)
+plt.plot(ts_log_diff)
+plt.plot(results_AR.fittedvalues, color='red')
+plt.title('RSS: %.4f'% sum((results_AR.fittedvalues-ts_log_diff)**2))
+
+#MA Model
+model = ARIMA(ts_log, order=(0, 1, 2))
+results_MA = model.fit(disp=-1)
+plt.plot(ts_log_diff)
+plt.plot(results_MA.fittedvalues, color='red')
+plt.title('RSS: %.4f'% sum((results_MA.fittedvalues-ts_log_diff)**2))
+
+#Combined Model
+model = ARIMA(ts_log, order=(2, 1, 2))
+results_ARIMA = model.fit(disp=-1)
+plt.plot(ts_log_diff)
+plt.plot(results_ARIMA.fittedvalues, color='red')
+plt.title('RSS: %.4f'% sum((results_ARIMA.fittedvalues-ts_log_diff)**2))
+
+#Taking it back to original scale
+predictions_ARIMA_diff = pd.Series(results_ARIMA.fittedvalues, copy=True)
+
+
+predictions_ARIMA_diff_cumsum = predictions_ARIMA_diff.cumsum()
+
+
+predictions_ARIMA_log = pd.Series(ts_log.ix[0], index=ts_log.index)
+predictions_ARIMA_log = predictions_ARIMA_log.add(predictions_ARIMA_diff_cumsum,fill_value=0)
+predictions_ARIMA_log.head()
+
+predictions_ARIMA = np.exp(predictions_ARIMA_log)
+plt.plot(ts)
+plt.plot(predictions_ARIMA)
+plt.title('RMSE: %.4f'% np.sqrt(sum((predictions_ARIMA-ts)**2)/len(ts)))
